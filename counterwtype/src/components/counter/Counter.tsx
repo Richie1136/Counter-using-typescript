473f6620b498642f082a83dc23406cc82514a68e
Richie Hagenah <69161935+Richie1136@users.noreply.github.com>
2022-01-13 11:57:15 -0500

    Created a basic counter using typescript

---
+++ counterwtype/src/components/counter/Counter.tsx
@@ -0,0 +1,22 @@
+import { useState } from "react";
+
+const Counter = () => {
+  const [count, setCount] = useState<number>(0);
+
+  const handleIncrement = () => {
+    setCount((prevCount) => prevCount + 1);
+  };
+
+  const handleDecrement = () => {
+    setCount((prevCount) => prevCount - 1);
+  };
+  return (
+    <div>
+      <h2>{count}</h2>
+      <button onClick={handleIncrement}>Increment</button>
+      <button onClick={handleDecrement}>Decrement</button>
+    </div>
+  );
+};
+
+export default Counter;
